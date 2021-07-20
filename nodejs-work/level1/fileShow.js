f2e8527fece3cd321b3e9ec7dd42cdae913b4a52
wind-yong <77865906+wind-yong@users.noreply.github.com>
2021-07-20 16:19:40 +0800

    Update fileShow.js

--- nodejs-work/level1/fileShow.js
+++ nodejs-work/level1/fileShow.js
@@ -4,29 +4,6 @@ const path = require("path")
 
 //读取文件，返回data和类型，根据类型修改报头
 
-// function fileShow(filePathName,ext){
-//     return new Promise(resolve => {
-//         fs.stat(filePathName,(err,stats)=>{
-//             if(err || !stats.isFile())
-//             {
-//                 resolve("not find file0")
-//             }
-//             if(!ext)   //文件没有后缀
-//             {
-//                 filePathName = path.join(__dirname,'./public',"index.html")
-//                 console.log(filePathName)
-//             }
-            
-//             fs.readFile(filePathName,(err,data)=>{
-//                 if(err)
-//                 {
-//                     resolve("not find file1")
-//                 }
-//                 resolve(data)
-//             })
-//         })
-//     })
-// }
 var fileShow = function(filePathName,ext){
     return new Promise(resolve => {
         if(fs.existsSync(filePathName))
